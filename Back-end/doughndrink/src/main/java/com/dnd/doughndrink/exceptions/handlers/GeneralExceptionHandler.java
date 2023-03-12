2e9278b2b8ca9e687d9f68c66600b5ca5a97d9bc
Sohaila Barakat <sohaila.barakat.sb@gmail.com>
2023-03-12 18:18:57 +0200

    jwt

--- Back-end/doughndrink/src/main/java/com/dnd/doughndrink/exceptions/handlers/GeneralExceptionHandler.java
+++ Back-end/doughndrink/src/main/java/com/dnd/doughndrink/exceptions/handlers/GeneralExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.dnd.doughndrink.dtos.ErrorResponse;
+import com.dnd.doughndrink.dtos.ResponseHandler;
 import com.dnd.doughndrink.exceptions.AlreadyExistException;
 import com.dnd.doughndrink.exceptions.UserNotFoundException;
 
@@ -15,32 +16,18 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class GeneralExceptionHandler {
     @ExceptionHandler(AlreadyExistException.class)
-    public ResponseEntity<ErrorResponse> handleAlreadyExistException(AlreadyExistException ex, HttpServletRequest req){
+    public ResponseEntity<Object> handleAlreadyExistException(AlreadyExistException ex, HttpServletRequest req){
         String message = String.format("%s", ex.getMessage());
-
-
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setMessage(message);
-        errorResponse.setStatus(HttpStatus.valueOf(409));
-        errorResponse.setCode(409);
-        errorResponse.setLocation(req.getRequestURI().toString());
-        return new ResponseEntity<>(errorResponse,errorResponse.getStatus());
+        return ResponseHandler.generateResponse(message, HttpStatus.valueOf(409), 409);
 
     }
 
 
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex, HttpServletRequest req){
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, HttpServletRequest req){
         String message = String.format("%s", ex.getMessage());
-
-
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setMessage(message);
-        errorResponse.setStatus(HttpStatus.valueOf(401));
-        errorResponse.setCode(401);
-        errorResponse.setLocation(req.getRequestURI().toString());
-        return new ResponseEntity<>(errorResponse,errorResponse.getStatus());
+        return ResponseHandler.generateResponse(message, HttpStatus.valueOf(401), 401);
 
     }
 }
