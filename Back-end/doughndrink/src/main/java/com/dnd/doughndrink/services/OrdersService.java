4d129e249197d0e1664ab85d413cd3432547b025
mina-yousry <minayousry597@gmail.com>
2023-03-15 09:51:17 +0200

    order updated

--- Back-end/doughndrink/src/main/java/com/dnd/doughndrink/services/OrdersService.java
+++ Back-end/doughndrink/src/main/java/com/dnd/doughndrink/services/OrdersService.java
@@ -4,13 +4,17 @@ package com.dnd.doughndrink.services;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dnd.doughndrink.dtos.OrderContainsProductDTO;
 import com.dnd.doughndrink.dtos.OrdersDTO;
+import com.dnd.doughndrink.mappers.OrderContainsProductMapper;
 import com.dnd.doughndrink.mappers.OrdersMapper;
 import com.dnd.doughndrink.mappers.UserMapper;
+import com.dnd.doughndrink.models.OrderContainsProduct;
 import com.dnd.doughndrink.models.Orders;
 import com.dnd.doughndrink.repositories.OrdersRepository;
 
@@ -27,6 +31,8 @@ import lombok.extern.slf4j.Slf4j;
 public class OrdersService {
 
     private final   OrdersRepository  ordersRepository;
+
+    private OrdersDTO ordersDTO;
 
     @Autowired
     private   final OrdersMapper ordersMapper;
@@ -60,5 +66,21 @@ public class OrdersService {
              List<Orders> orders= ordersRepository.findAllByUserUserId(userId);
              return ordersMapper.map(orders); 
           }
-            
+    //  public double totalPrice(){       
+    //       double totalPrice = 0D;
+    //       Set<OrderContainsProduct> orderContainsProducts = orders.getOrderContainsProducts();
+    //       for (OrderContainsProduct op : orderContainsProducts){
+    //           totalPrice += (op.getProduct().getPrice()*op.getQuntity());
+    //       }
+    //       return totalPrice;
+    //      }
+
+         public double totalPrice(){       
+          double totalPrice = 0D;
+          Set<OrderContainsProductDTO> orderContainsProducts = ordersDTO.getOrderContainsProducts();
+          for (OrderContainsProductDTO op : orderContainsProducts){
+              totalPrice += (op.getProduct().getPrice()*op.getQuntity());
+          }
+          return totalPrice;
+         }
 }
