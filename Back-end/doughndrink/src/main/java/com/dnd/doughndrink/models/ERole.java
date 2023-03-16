39c0b263c94a2eb9d2ba215688ef708eb0305074
SohailaBarakat <83607733+SohailaBarakat@users.noreply.github.com>
2023-03-16 04:21:50 +0200

    Delete Back-end directory

--- Back-end/doughndrink/src/main/java/com/dnd/doughndrink/models/ERole.java
+++
@@ -1,6 +0,0 @@
-// package com.dnd.doughndrink.models;
-
-// public enum ERole {
-//     ROLE_USER,
-//     ROLE_ADMIN, ROLE_MODERATOR
-// }
--- Back-end/doughndrink/src/main/java/com/dnd/doughndrink/repositories/OrderContainsProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dnd.doughndrink.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dnd.doughndrink.models.OrderContainsProduct;
-
-public interface OrderContainsProductRepository extends JpaRepository<OrderContainsProduct, Integer> {
-    
-}
